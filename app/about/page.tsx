771cfeee1e027c5cbb40068873ab50356c363191
Jo <johj703@nate.com>
2024-03-31 04:33:29 +0900

    About 페이지 생성

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <h3>
+        Welcome to the official explorer for The New York Times Best Seller list
+        explorer.
+      </h3>
+      <h3>We hope you enjoy your stay!</h3>
+    </div>
+  );
+}
