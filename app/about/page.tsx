d7560e1befe55d773804a0c351bab9ad493fdd1f
Jo <johj703@nate.com>
2024-03-31 17:20:33 +0900

    About.tsx 페이지에 Navigation 컴포넌트 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,9 @@
+import Navigation from "../../components/navigation";
+
 export default function About() {
   return (
     <div>
+      <Navigation />
       <h1>About</h1>
       <p>
         Welcome to the official explorer for The New York Times Best Seller list
