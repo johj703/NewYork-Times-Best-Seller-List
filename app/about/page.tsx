9a7475b1aa66afe6c8d3c16a9d5735ec855df4bd
Jo <johj703@nate.com>
2024-03-31 18:38:14 +0900

    app/page.tsx, app/about/page.tsx에 있는 Navigation 컴포넌트를 삭제하고 layout.tsx에 Navigation 컴포넌트를 입력

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../../components/navigation";
-
 export default function About() {
   return (
     <div>
-      <Navigation />
       <h1>About</h1>
       <p>
         Welcome to the official explorer for The New York Times Best Seller list
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../components/navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The New York Times Best Seller Explorer</h1>
     </div>
   );
