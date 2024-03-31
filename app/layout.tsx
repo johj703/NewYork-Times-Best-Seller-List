cc154ff93b362a074b3a26a1151302e8a643a40a
Jo <johj703@nate.com>
2024-03-31 19:30:00 +0900

    RootLayout을 Layout으로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Navigation from "../components/navigation"
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>
