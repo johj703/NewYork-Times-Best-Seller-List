b9a11a5317a5647a3a54de3989fac9f63180e76a
Jo <johj703@nate.com>
2024-03-31 23:57:26 +0900

    metadata를 다시 만들어서 server 컴포넌트 작성

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,20 @@
-"use client";
-import { useEffect, useState } from "react";
+export const metadata = {
+  title: "Home",
+};
+
+const URL = "https://books-api.nomadcoders.workers.dev/lists";
+
+async function getLists() {
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
 export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [lists, setLists] = useState([]);
-  const getList = async () => {
-    const response = await fetch(
-      "https://books-api.nomadcoders.workers.dev/lists"
-    );
-    const json = await response.json();
-    setLists(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getList();
-  });
   return (
     <div>
       <h1>The New York Times Best Seller Explorer</h1>
-      {isLoading ? "Loading!" : JSON.stringify(lists)}
+      {JSON.stringify(lists)}
     </div>
   );
 }
