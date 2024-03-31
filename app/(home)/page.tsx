4433d0876e632e68f2c8bd3c56e311fe1784a3b4
Jo <johj703@nate.com>
2024-03-31 21:21:34 +0900

    book List 불러오는 useState, useEffect 작성(api 주소 입력하기 전)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
+
 export const metadata = {
   title: 'Home',
 }
 
 export default function Page() {
+  const [movies, setMovies] = useState();
+  const getList = async () => {
+    fetch("")
+  }
+  useEffect(() => {
+    getList();
+  })
   return (
     <div>
       <h1>The New York Times Best Seller Explorer</h1>
