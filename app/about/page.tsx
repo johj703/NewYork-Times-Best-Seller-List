17d67e0991e2d301abd9911053f2b5674c6e8220
Jo <johj703@nate.com>
2024-03-31 17:13:17 +0900

    h3 태그를 p태그로 변경

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,11 +2,11 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <h3>
+      <p>
         Welcome to the official explorer for The New York Times Best Seller list
         explorer.
-      </h3>
-      <h3>We hope you enjoy your stay!</h3>
+        We hope you enjoy your stay!
+      </p>
     </div>
   );
 }
